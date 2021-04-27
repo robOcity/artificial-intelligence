e2a62bbff99b460fc8362fa9913dbcd4e3edf158
robOcity <robert.osterburg@gmail.com>
2021-04-27 15:52:15 -0600

    code: build_table passing tests

--- Exercises/2d_opening_book/openingbook.py
+++ Exercises/2d_opening_book/openingbook.py
@@ -6,16 +6,38 @@ NUM_ROUNDS = 10
 
 
 def build_table(num_rounds=NUM_ROUNDS):
-    # You should run no more than `num_rounds` simulations -- the
-    # goal of this quiz is to understand one possible way to develop
-    # an opening book; not to develop a good one
+    """Build a basic opening book of moves."""
 
-    # NOTE: the GameState object is not hashable, and the python3
-    #       runtime includes security features that make object
-    #       hashes non-portable. There is a new attribute on
-    #       GameState objects in this quiz called `hashable` that
-    #       can be used as a dictionary key
+    import random
 
-    # TODO: return a table {k:v} where each k is a game state
-    #       and each v is the best action to take in that state
-    return {}
+    initial_moves = [
+        (2, 2),
+        (1, 1),
+        (0, 0),
+        (0, 2),
+        (2, 0),
+        (1, 2),
+        (2, 1),
+        (3, 3),
+        (3, 1),
+        (1, 3),
+    ]
+    openingbook = {}
+    for i, move in enumerate(initial_moves):
+        game = GameState()
+        liberties = game.liberties(game._player_locations[game.player()])
+        if move not in liberties:
+            move = random.choice(liberties)
+        game = game.result(move)
+        openingbook[game.hashable] = move
+    return openingbook
+
+
+def h_moves_diff(gamestate):
+    """
+    The differential in moves available to the current player.  Positive values
+    are better than negative values for this evaluation heuristic.  
+    """
+    my_loc = gamestate._player_locations[gamestate.player()]
+    op_loc = gamestate._player_locations[gamestate.player() ^ 1]
+    return gamestate.liberties(my_loc) - gamestate.liberties(op_loc)
